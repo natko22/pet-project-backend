348b2ae0b08b0984afb40ccf29a9ff4da0858ebb
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-24 22:54:34 +0200

    add upload route folder

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const uploadRoutes = require("./routes/upload.routes");
+app.use("/api", uploadRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
