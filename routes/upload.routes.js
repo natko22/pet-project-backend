3b1786c459a2e0a04f50496ea43d1e936c67ba1e
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-24 23:02:26 +0200

    add upload route and installed cloudinary package

---
+++ routes/upload.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const photoUploader = require("../config/cloudinary.config");
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", photoUploader.single("imageUrl"), (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+
+  res.json({ fileUrl: req.file.path });
+});
+
+module.exports = router;
