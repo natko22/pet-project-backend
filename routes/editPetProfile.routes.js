8b982a3ded1ba36593d6b08704f30152cf5e2e70
Natassa <112552395+natko22@users.noreply.github.com>
2023-07-01 09:16:53 +0200

    correct edit pet route

--- routes/editPetProfile.routes.js
+++ routes/editPetProfile.routes.js
@@ -8,14 +8,15 @@ const Pet = require("../models/Pet.model");
 router.get("/edit-pet/:_id", async (req, res) => {
   try {
     const petId = req.params._id;
-    // console.log(req.payload);
     const pet = await Pet.findById(petId);
+    console.log(petId, "PET-ID");
 
     if (!pet) {
       return res.status(404).json({ message: "Pet not found" });
     }
 
     res.status(200).json({ pet });
+    console.log("PET OBJECT", pet);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong" });
@@ -25,7 +26,7 @@ router.get("/edit-pet/:_id", async (req, res) => {
 // update pets's profile
 router.put("/edit-pet/:_id", async (req, res) => {
   try {
-    const petId = req.payload._id;
+    const petId = req.params._id;
     const updatedPetProfile = req.body;
 
     const pet = await Pet.findByIdAndUpdate(petId, updatedPetProfile, {
@@ -38,6 +39,7 @@ router.put("/edit-pet/:_id", async (req, res) => {
     }
 
     res.json(pet);
+    console.log("PET", pet);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
